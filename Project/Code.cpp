e40de5bada917cf7020eb886f9b11520a43a5def
TheBestLogic <alex0isher@gmail.com>
2018-02-05 23:26:54 +0200

    Добавил метод шифрования. Еще не тестил.

--- Project/Code.cpp
+++ Project/Code.cpp
@@ -41,5 +41,34 @@ void Code::Set_StringToCode(string &InString)
 
 void Code::Create_ShifrCode()
 {
-
+	int Length = StringToCode->length;
+	string Str = *StringToCode;
+	// first step
+	if (Length % 2)
+	{
+		for (int i = 0, j = Length - 1; i < Length || j > 0; ++i, --j)
+		{
+			if (i != j)
+			{
+				Str[i] ^= Str[j];
+			}
+			else
+			{
+				~Str[i];
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0, j = Length - 1; i < Length || j > 0; ++i, --j)
+		{
+			Str[i] ^= Str[j];
+		}
+	}
+	// second step
+	for (int i = 0, char j = ~Str[Length/2]; i < Length; ++i)
+	{
+		Str[i] ^= j;
+	}
+	*ShifrCode = Str;
 }
